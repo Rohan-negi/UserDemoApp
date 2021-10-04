9efb49619f214ab69d7666d41c2de2f5295d156e
Rohan-negi <91666469+Rohan-negi@users.noreply.github.com>
2021-10-05 01:22:12 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	private Integer age;
+	@OneToOne(cascade = CascadeType.ALL)
+	Address address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
